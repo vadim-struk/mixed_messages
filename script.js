eccb1ab793a75fd0624e03b79f6659683731f906
Vadim <gotolviv@gmail.com>
2023-01-26 17:47:05 +0200

    Added main function

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 function randMessage(){
-    const messageAdjectives = ['Smiling', 'Dancing', 'Crying', 'Drunk', 'Sleeping', 'Walking']
-    const messageSubject = ['me', 'mom', 'dog', 'pope', 'horse', 'kid']
-    const messageAction = ['talking to', 'singing with', 'robbing', 'praying to', 'having a bad trip with']
-    const messageObject = ['frog', 'beggar', 'flower', 'computer', 'pedophile', 'alien', 'Elon Musk']
+    const messageAdjectives = ['smiling', 'dancing', 'crying', 'drunk', 'sleeping', 'walking']
+    const messageSubjects = ['me', 'mom', 'dog', 'pope', 'horse', 'kid']
+    const messageActions = ['talking to', 'singing with', 'robbing', 'praying to', 'having a bad trip with']
+    const messageObjects = ['frog', 'beggar', 'flower', 'computer', 'pedophile', 'alien', 'Elon Musk']
     
     let randAdjective = messageAdjectives[Math.floor(Math.random()*messageAdjectives.length)]
-    
-   // console.log("test");
-    console.log(randAdjective);     
+    let randSubject = messageSubjects[Math.floor(Math.random()*messageSubjects.length)]
+    let randAction = messageActions[Math.floor(Math.random()*messageActions.length)]
+    let randObject = messageObjects[Math.floor(Math.random()*messageObjects.length)]
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+
+    let finalMessage = `One shiny day a ${randAdjective} ${randSubject} was ${randAction} ${vowels.includes(randObject.slice(0,1).toLowerCase()) ? 'an' : 'a'} ${randObject}`
+
+    //console.log(messageObjects.slice(0,1))
+    console.log(vowels.includes(messageObjects.slice(0,1)) ? 'an' : 'a');
+    console.log(finalMessage);     
 
 }
 
